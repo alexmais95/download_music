022553353d335d69c450d92adab3d59d6dbada91
alexmais95 <112903498+alexmais95@users.noreply.github.com>
2023-09-01 12:02:47 +0300

    add function for find buttom and push for download.

--- main.py
+++ main.py
@@ -2,18 +2,20 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import lxml
+from setting import *
+import json
+import time
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 
-url = 'https://drivemusic.club/novinki_muzyki'
-header = {
-    'Accept': '*/*',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
-}
-req = requests.get(url, headers=header)
-
-site_text = req.text
-
-with open('site_drive.html', 'w', encoding='utf-8') as f:
-     file = f.write(site_text)
+def make_html(url, header, name_file):
+    req = requests.get(url, headers=header)
+    site_text = req.text
+    with open(f'{name_file}.html', 'w', encoding='utf-8') as f:
+        file = f.write(site_text)
+    return file
 
 
 def link_for_downlod():
@@ -34,4 +36,14 @@ def link_for_downlod():
     with open('music_linc1.json', 'w', encoding='utf-8') as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
 
-link_for_downlod()
+def download_music(file_name):
+    with open(f'{file_name}.json', encoding='utf-8') as f:
+        file = json.load(f)
+    for el in file:
+        browser = webdriver.Firefox()
+        browser.maximize_window()
+        browser.get(el)
+        time.sleep(5)
+
+        find = browser.find_element(By.XPATH, '//a[@class="song-author-btn btn-download"]')
+        find.click()
