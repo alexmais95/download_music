7e9ce3b50992c5e47ea90808e5b4a79d1f2c53fa
alexmais95 <112903498+alexmais95@users.noreply.github.com>
2023-09-01 18:26:44 +0300

    create class Make_Download

--- main.py
+++ main.py
@@ -7,34 +7,31 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-def make_html(url, header, name_file):
-    req = requests.get(url, headers=header)
-    site_text = req.text
-    with open(f'{name_file}.html', 'w', encoding='utf-8') as f:
-        file = f.write(site_text)
-    return file
+class Make_Download:
+    def __init__(self, url, header):
+        self.url = url
+        self.header = header
+
+    def download(self, url, header):
+        req = requests.get(url, headers=header)
+        site_text = req.text
+        soup = BeautifulSoup(site_text, 'lxml')
+        find_el = soup.find_all(class_='popular-play-author')
+
+        for element in find_el:
+            a_href = element.get('href')
+            el = 'https://drivemusic.club' + a_href
+            browser = webdriver.Chrome()
+            browser.maximize_window()
+            browser.get(el)
+            find = browser.find_element(By.CLASS_NAME, 'add_word')
+            find.click()
+            time.sleep(5)
+
+    def create_download(self):
+        for site in self.url:
+            self.download(site, self.header)
 
 
-def downlod(url, header):
-    req = requests.get(url, headers=header)
-    site_text = req.text
-    soup = BeautifulSoup(site_text, 'lxml')
-    find_el = soup.find_all(class_='popular-play-author')
-
-    for element in find_el:
-        a_href = element.get('href')
-        el = 'https://drivemusic.club' + a_href
-        browser = webdriver.Chrome()
-        browser.maximize_window()
-        browser.get(el)
-        find = browser.find_element(By.CLASS_NAME, 'add_word')
-        find.click()
-        time.sleep(5)
-
-
-for site in url:
-    downlod(site, header)
-
-
-
-
+# download = Make_Download(url, header)
+# download.create_download()
