7b5758bcd89f0651af72064761038f435a646127
alexmais95 <112903498+alexmais95@users.noreply.github.com>
2023-09-01 18:00:30 +0300

    make function, that take url and header, search link of song, and click for download.

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
-import json
 import lxml
 from setting import *
-import json
 import time
-from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
+
 
 def make_html(url, header, name_file):
     req = requests.get(url, headers=header)
@@ -18,32 +15,26 @@ def make_html(url, header, name_file):
     return file
 
 
-def link_for_downlod():
-    with open('site_drive.html', encoding='utf-8') as f:
-         site = f.read()
-
-    soup = BeautifulSoup(site, 'lxml')
-
+def downlod(url, header):
+    req = requests.get(url, headers=header)
+    site_text = req.text
+    soup = BeautifulSoup(site_text, 'lxml')
     find_el = soup.find_all(class_='popular-play-author')
 
-    data = []
     for element in find_el:
         a_href = element.get('href')
         el = 'https://drivemusic.club' + a_href
-        data.append(el)
-
-
-    with open('music_linc1.json', 'w', encoding='utf-8') as f:
-        json.dump(data, f, indent=4, ensure_ascii=False)
-
-def download_music(file_name):
-    with open(f'{file_name}.json', encoding='utf-8') as f:
-        file = json.load(f)
-    for el in file:
-        browser = webdriver.Firefox()
+        browser = webdriver.Chrome()
         browser.maximize_window()
         browser.get(el)
+        find = browser.find_element(By.CLASS_NAME, 'add_word')
+        find.click()
         time.sleep(5)
 
-        find = browser.find_element(By.XPATH, '//a[@class="song-author-btn btn-download"]')
-        find.click()
+
+for site in url:
+    downlod(site, header)
+
+
+
+
---
+++ setting.py
@@ -0,0 +1,10 @@
+header = {
+    'Accept': '*/*',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+}
+
+
+url =[
+    'https://drivemusic.club/best_music.html',
+    'https://drivemusic.club/rock_music/order/pop/',
+]
